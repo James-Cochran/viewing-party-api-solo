2dcc5f3d5912b8b5a06da09840dfe1762d84f53e
James-Cochran <jameslikesfruit@gmail.com>
2024-12-05 19:22:09 -0500

    gross logic in the controller to get movie query test to work. refactor some tests expectations to align correctly, added lord of the rings fixture file, and made adjustments to test into that.

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::MoviesController < ApplicationController
   def index
     api_key = ENV['THEMOVIEDB_KEY'] || Rails.application.credentials.themoviedb[:key]
-    response = Faraday.get("https://api.themoviedb.org/3/movie/top_rated", {
-      api_key: api_key })
+
+    if params[:query]
+      response = Faraday.get("https://api.themoviedb.org/3/search/movie", {
+        api_key: api_key,
+        query: params[:query]
+      })
+    else
+      response = Faraday.get("https://api.themoviedb.org/3/movie/top_rated", {
+        api_key: api_key
+      })
+    end
+
     movies = JSON.parse(response.body, symbolize_names: true)[:results]
 
     formatted_movies = movies.first(20).map do |movie|
@@ -18,5 +28,4 @@ class Api::V1::MoviesController < ApplicationController
 
     render json: { data: formatted_movies }, status: :ok
   end
-
 end
--- spec/requests/api/v1/movies_request_spec.rb
+++ spec/requests/api/v1/movies_request_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe 'Movies Endpoints', type: :request do
 
     describe "Movie Search" do
       it "can retrieve a list of movies based on a search query" do
-        json_response = File.read('spec/fixtures/top_rated_movies.json')
+        json_response = File.read('spec/fixtures/lord_of_the_rings.json')
         stub_request(:get, "https://api.themoviedb.org/3/search/movie")
           .with(query: { api_key: Rails.application.credentials.themoviedb[:key], query: "Lord of the Rings" })
           .to_return(status: 200, body: json_response)
@@ -34,12 +34,13 @@ RSpec.describe 'Movies Endpoints', type: :request do
         json = JSON.parse(response.body, symbolize_names: true)[:data]
   
         expect(json.count).to be <= 20 
-        movie = json.first
-        expect(movie[:type]).to eq("movie")
-        expect(movie[:id]).to be_a(String)
-        expect(movie[:attributes]).to have_key(:title)
-        expect(movie[:attributes][:title]).to eq("The Lord of the Rings: The Return of the King")
-        expect(movie[:attributes]).to have_key(:vote_average)
+        
+        first_movie = json.first
+        expect(first_movie[:attributes][:title]).to eq("The Lord of the Rings: The Fellowship of the Ring")
+        expect(first_movie[:type]).to eq("movie")
+        expect(first_movie[:id]).to be_a(String)
+        expect(first_movie[:attributes]).to have_key(:title)
+        expect(first_movie[:attributes]).to have_key(:vote_average)
       end
     end
   end
