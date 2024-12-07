14655f5f30b29206d467bb00817a266df0e63014
James-Cochran <jameslikesfruit@gmail.com>
2024-12-07 16:06:37 -0500

    created viewing party table with migration. created viewing party model with validations of table headers.

---
+++ app/models/viewing_party.rb
@@ -0,0 +1,7 @@
+class ViewingParty < ApplicationRecord
+  validates :name, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :movie_id, presence: true
+  validates :movie_title, presence: true
+end
